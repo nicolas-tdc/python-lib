011fa3423c0f1147c0eb2c9d3d31401b5c58bea4
nicolas.tdc <nicolas.tachetdescombes@gmail.com>
2021-10-27 15:14:28 +0200

    Remove prime numbers

--- numerics.py
+++ numerics.py
@@ -9,25 +9,3 @@ def is_leap_year(year):
         return True
     else:
         return False
-
-
-import math
-
-def prime_factors(value):
-    """
-    :param value: Int
-    :return: List - Prime factors of given value
-    """
-    prime_factors = []
-    while value % 2 == 0:
-        value = update_list_and_value(value, 2, prime_factors)
-
-    value_sqrt = int(math.sqrt(value)) + 1
-    for odd in range(3, value_sqrt):
-        while value % odd == 0:
-
-            value = update_list_and_value(value, odd, prime_factors)
-    if value != 1:
-        prime_factors.append(value)
-
-    return prime_factors
